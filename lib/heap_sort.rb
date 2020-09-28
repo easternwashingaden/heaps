9a71d831a77554d3f942159fbc0cf905d4396a29
tithvorlakmok <tithvorlakmok@gmail.com>
2020-09-27 17:01:35 -0700

    Passed all the tests

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,24 @@
 
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity: O(logn)
+# Space Complexity: O(n)
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  return [] if list == []
+  return list if list.length == 1
+  # list = [5, 27, 3, 16, -50]
+  # must_equal [-50, 3, 5, 16, 27]
+
+  heap = MinHeap.new
+  list.each do |element|
+    heap.add(element, element)
+  end
+
+  result = []
+  i = 0 
+  while i < list.length
+    result << heap.remove
+    i += 1
+  end
+  return result
 end
--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,18 +14,32 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(logn) - we just look at the sub children only
+  # Space Complexity: O(1)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    # New node is added to the end (last element of the array)
+    @store << HeapNode.new(key, value)
+    last_index = @store.length - 1
+    heap_up(last_index)  #Using heap_up helper method 
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
-  def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+  # Time Complexity: O(logn)
+  # Space Complexity: O(1)
+  def remove() # removes the element at the first position (root) of the array
+    return nil if @store.empty?
+    last_index = @store.length - 1
+    # First, swap the last leaf & the root node
+    swap(0, last_index)
+
+    # Delete the last leaf (last item in the array)
+    result = @store.pop
+
+    # Then heap-down the new root, to reestablish the heap property
+    # start heap_down with the root (index 0)
+    heap_down(0) 
+    return result.value
   end
 
 
@@ -44,10 +58,10 @@ class MinHeap
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(1)
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return if @store.empty?
   end
 
   private
@@ -55,17 +69,44 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(logn)
+  # Space complexity: O(1)
   def heap_up(index)
-    
+    return if index == 0
+    parent = (index - 1 ) / 2
+    # Compare the new node with it's parent
+    # If they are out of order swap and heap-up
+    if @store[parent].key > @store[index].key
+      #swap
+      swap(index, parent)
+      #continue heap-up
+      heap_up(parent)
+    end
+ 
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left_child_index = index * 2 + 1
+    right_child_index = index * 2 + 2
+    #check to see if the right child exists
+    if @store[right_child_index] != nil
+      # Define the min_child
+      min_child = @store[right_child_index].key < @store[left_child_index].key ? right_child_index : left_child_index
+      # if the min_child less than the parent, swap it and continue heaping down
+      if @store[min_child].key < @store[index].key
+        swap(min_child, index)
+        heap_down(min_child)
+      end
+    elsif @store[left_child_index] != nil #check to see if the left child exists
+      if @store[left_child_index].key < @store[index].key #if the min_child less than the parent, swap it and continue heaping down
+        swap(left_child_index, index)
+        heap_down(left_child_index)
+      end
+    end
+     
   end
 
   # If you want a swap method... you're welcome
